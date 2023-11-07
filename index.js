80028638bdc39111dfc01e8d6ca9f4c4bd77178b
Mercyfulll <mercyful.buthelezi@gmail.com>
2023-11-07 15:29:35 +0200

    second commit

--- index.js
+++ index.js
@@ -1,40 +1,22 @@
 import express from 'express';
+import cors from 'cors';
 import {engine} from "express-handlebars";
 import bodyParser from "body-parser";
 import pgPromise from "pg-promise";
-import flash from "express-flash";
-import session from "express-session";
 import shoesAPI from './api/shoe-api.js';
 import shoeQueries from './services/shoe-database.js';
-import routes from './routes/routes.js';
-
-
 
 var app = express()
 var pgp = pgPromise();
 
-
 var connectionString = process.env.DATABASE_URL || 'postgres://nkndnlfv:v1L6kH69CUXaXSjXIBfO-82KUL3gOQ-c@tyke.db.elephantsql.com/nkndnlfv'
-
 var db = pgp(connectionString)
 
 var shoedb = shoeQueries(db)
-var route = routes(db)
-
 var shoeapi = shoesAPI(shoedb)
-
 
 // use the express.static built-in middleware to serve static file 'css'
 app.use(express.static(('public')))
-
-//use session to maintain data on the application
-app.use(session({
-    secret : 'This is a string',
-    resave: false,
-    saveUninitialized: true
-}));
-
-app.use(flash());
 
 // set and callback engine 
 app.engine('handlebars', engine());
@@ -45,14 +27,12 @@ app.set('views', './views');
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
+  // If you want to allow requests from any domain, you can use:
+  app.use(cors());
 
-app.get('/',route.home)
-app.post('/shoes/brand/:brandname',route.filterBrand)
-app.post('/shoes/size/:size',route.filterSize)
-app.post('/shoes/brand/:brandname/size/:size',route.filterBrandSize)
 
 // Api routes 
-app.post('	/api/shoes/sold/:id',function(req,res){
+app.post('/api/shoes/sold/:id',function(req,res){
     res.render('index')
 })
 app.get('/api/shoes/brand/:brandname/size/:size',shoeapi.getShoeByBrandAndSize)
